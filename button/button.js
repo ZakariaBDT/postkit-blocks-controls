4bd2257d5ea57d93668cbde9ae211e709ce0f1cc
ZakariaBDT <zakaria@bdthemes.com>
2023-03-09 09:56:42 +0600

    Simple button component

---
+++ button/button.js
@@ -0,0 +1,17 @@
+/**
+ * WordPress dependencies
+ */
+
+// import style 
+import './button.scss';
+
+
+const Button = () => {
+    return (
+        <button className="button">
+            I am a button from Github Component Library
+        </button>
+    )
+};
+
+export default Button;
